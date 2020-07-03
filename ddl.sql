c10c4f83c606ac23e88dbe4835e23b006152cb9f
chenmianhan <mianhan1998@139.com>
2020-07-03 17:00:53 +0800

    finished

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,31 @@
 -- **Database level:**
 -- Display all the database
-
+show databases;
 -- Enter a certain database
-
+use   dml_ddl;
 -- Create a database
-
+create database dml_ddl;
 -- Create the database of the designated character set
-
+drop database if exists dml_ddl;
+create database dml_ddl character set utf8;
 -- Display the creation information fo the database
-
+show create database dml_ddl;
 -- Revise the codes of the database
-
+alter database dml_ddl character set gb2312;
 -- Delete a database
-
+drop database if exists dml_ddl;
 -- **Table level**
 -- Revise table name
-
+alter table tl1 rename student;
 -- Revise the field's data type
-
+alter table student modify name1 varchar(50);
 -- Revise field name
-
--- Add field
-
+alter table student change name1 name varchar(15);
 -- Delete field
-
+alter table student drop name;
 -- Revise the table's storage engine
-
+alter table student engine=MyISAM;
 -- Delete the table's foreign key restriant
-
+alter table student drop foreign key fk_name;
 -- Delete a table
+drop table student;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,9 @@
 -- Insert record
-
+insert into student(id,name,age,sex) values('001','Colin','18','male');
+insert into student(id,name,age,sex) values('002','Emma','20','female');
 -- Revise record
-
+UPDATE  student SET age='19'WHERE id='001';
 --  Delete record
-
+DELETE From student where id='002';
 -- Search record
+SELECT * from student where id='001';
